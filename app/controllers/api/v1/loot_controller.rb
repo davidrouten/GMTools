6d2465c72941a967bd2e830723a2b076787fe023
David R <davidellisr@yahoo.com>
2017-03-19 10:52:49 -0400

    try this to hit the incoming webhook

--- app/controllers/api/v1/loot_controller.rb
+++ app/controllers/api/v1/loot_controller.rb
@@ -1,8 +1,19 @@
 class Api::V1::LootController < ApplicationController
+  require 'net/http'
+  require 'uri'
+
   def index
-    render json: {
-      text: 'Index works, not used for anything.',
-    }
+    uri = URI.parse("https://hooks.slack.com/services/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")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
+    request.set_form_data({
+      text: 'THIS IS IT! SUCCESS!',
+    })
+
+    response = http.request(request)
+    render :json => response.body
   end
 
   def create
